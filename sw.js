f1b06b43c1e5eeacc88d7f77ba9e2997ffca7bea
IMRAN AHMED <imran.info.me@gmail.com>
2026-07-07 16:36:50 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -77,15 +77,11 @@ self.addEventListener('fetch', (event) => {
     return;
   }
 
-  
 
-
-
-
-
-
-
-
+if (url.href.startsWith(AUDIO_CDN)) {
+    event.respondWith(cacheFirstAudio(req));
+    return;
+  }
 
   if (url.href.startsWith(API)) {
     event.respondWith(networkFirst(req, API_CACHE_NAME));
